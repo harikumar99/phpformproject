2e35e4c3381b1c7873d803d9a9ee7ce1f5a7f802
harikumar <harikumar@oxsoftwares.com>
2017-07-25 12:37:39 +0530

    connection query added and its common

---
+++ connect-mysql.php
@@ -0,0 +1,14 @@
+<?php
+ define('DB_USER', 'root');
+ define('DB_PSWD', 'admin@123');
+ define('DB_HOST', 'localhost');
+ define('DB_NAME', 'registration');
+ 
+ 
+ $dbcon = mysqli_connect(DB_HOST, DB_USER,DB_PSWD,DB_NAME);
+ 
+ 
+?>
+
+
+
