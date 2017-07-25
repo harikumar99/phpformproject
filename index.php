cfafe3d8f42c8caaf28708a125fcf64326754453
harikumar <harikumar@oxsoftwares.com>
2017-07-25 14:44:24 +0530

    Externel style sheet is added

--- index.php
+++ index.php
@@ -10,59 +10,7 @@ and open the template in the editor.
         <title>Welcome</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            .reg1{
-                width:45%;
-                height:300px;
-
-                margin:auto;
-                background-color:	#FFFACD;
-                box-shadow:  0px 0px 30px rgba(0, 0, 0, 0.22);
-
-            }
-            h1{
-                background:	#F4A460;
-                text-transform: uppercase;
-                color: white;
-                text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;
-                font-family: 'Open Sans Condensed', sans-serif;
-                font-size: 18px;
-                text-align: center;
-                font-weight: 100;
-                padding: 20px;
-
-            }
-
-            button           {  
-                background-color:#000000;
-                margin-top:10px;
-                color: #FFFFFF;
-                width:20%;
-                height:50px;	
-                border-radius:50px;
-                margin-left:150px;
-                font-family: 'Open Sans Condensed', sans-serif;
-                font-size: 18px;
-                text-transform: uppercase;
-            }
-            a{
-                text-decoration: none;
-                 color: #FFFFFF;
-            }
-            #userdetails{
-                
-                margin-top:20px;
-                margin-left: 20px;
-                
-            }
-
-
-
-
-        </style>
-
-
-
+        <link rel="stylesheet" href="css/formstyle.css">
     </head>
     <body>
         <div class="reg1">
@@ -95,7 +43,7 @@ and open the template in the editor.
                      $address = $rows['user_address'];
                      $date= $rows['user_date'];
                     
-                    echo "<tr style='color:navy;'><td> $id</td>";
+                    echo "<tr><td> $id</td>";
                     echo "<td> $name</td>";
                     echo "<td> $email</td>";
                     echo "<td> $dob</td>";
