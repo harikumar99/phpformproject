948de8260440dd7a8865c4c67478a2fdd367d028
harikumar <harikumar@oxsoftwares.com>
2017-07-27 18:39:05 +0530

    Session added

--- insert.php
+++ insert.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if($_SESSION['username'])
+{
+    header('location:userinformation.php');
+}
+?>
 <html>
     <head>
         <title>Welcome</title>
@@ -13,7 +20,7 @@
                 box-shadow:  0px 0px 30px rgba(0, 0, 0, 0.22);
 
             }
-            
+
             h2{
                 margin-top:150px;
             }
@@ -25,36 +32,51 @@
     <body>
         <div class="reg1">   
 
-<div id='respnce'>
+            <div id='respnce'>
 
-            <?php
-            if ($_POST[submit]) {
-                include ('connect-mysql.php');
-                
-                $name = $_POST[name];
-                $password =md5($_POST[pwd]);
-                $email = $_POST[email];
-                $dob = $_POST[dob];
-                $mobile = $_POST[mobile];
-                $gender = $_POST[gender];
-                $address = $_POST[address];
-                $target = "upload/";
-                $pict = $target.$_FILES['pic']['name'];
-                move_uploaded_file($_FILES['pic']['tmp_name'], $target.$_FILES['pic']['name']);
-                $date = date('Y-m-d');
+                <?php
+                if ($_POST[submit]) {
+                    include ('connect-mysql.php');
 
-                $sqlinsert = "INSERT INTO user_details(user_name,user_password,user_email,user_dob,user_mobile,user_gender,user_address,user_pic,user_date)VALUES('$name','$password','$email','$dob','$mobile','$gender','$address','$pict','$date')";
+                    $name = $_POST[name];
+                    $password = md5($_POST[pwd]);
+                    $email = $_POST[email];
+                    $dob = $_POST[dob];
+                    $mobile = $_POST[mobile];
+                    $gender = $_POST[gender];
+                    $address = $_POST[address];
+                    $target = "upload/";
+                    $pict = $target . $_FILES['pic']['name'];
+                    move_uploaded_file($_FILES['pic']['tmp_name'], $target . $_FILES['pic']['name']);
+                    $date = date('Y-m-d');
+                    
+                    $select = "SELECT user_name,user_password FROM user_details";
+                    $result = mysqli_query($dbcon, $select) or die('error');
+                    while ($rows = mysqli_fetch_assoc($result)) {
+                        $dbusername = $rows['user_name'];
+                        $dbpassword = $rows['user_password'];
+                        if ($dbusername == $name) {
+                            echo "<h2>User name alreday exists</h2>";
+                            echo "<a href='register.php'>Click here</a> to return  Register page";
+                        } else if ($dbpassword == $password) {
+                            echo "<h2>This password is already taken try another</h2>";
+                            echo "<a href='register.php'>Click here</a> to return  Register page";
+                        } else {
 
-                if (!mysqli_query($dbcon, $sqlinsert)) {
+                            $sqlinsert = "INSERT INTO user_details(user_name,user_password,user_email,user_dob,user_mobile,user_gender,user_address,user_pic,user_date)VALUES('$name','$password','$email','$dob','$mobile','$gender','$address','$pict','$date')";
 
-                    die("Error in inserting new record");
-                } else {
-                    echo "<h2>Registration Successfully Done !!</h2> <br>";
-                    echo "<a href='login.php'>Click here</a> to return  Login page";
+                            if (!mysqli_query($dbcon, $sqlinsert)) {
+
+                                die("Error in inserting new record");
+                            } else {
+                                echo "<h2>Registration Successfully Done !!</h2> <br>";
+                                echo "<a href='login.php'>Click here</a> to return  Login page";
+                            }
+                        }
+                    }
                 }
-            }
-            ?>
-        </div>
+                ?>
+            </div>
         </div>
 
     </body>
