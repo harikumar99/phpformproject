72097e8d2ffcd907b59589acc19d0e2c2ce71fd0
harikumar <harikumar@oxsoftwares.com>
2017-07-27 18:48:07 +0530

    Edit button removed

--- userinformation.php
+++ userinformation.php
@@ -1,6 +1,13 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/userinfostyle.css">
+        <style>
+        img{
+            width:200px;
+            height:200px;
+          
+        }
+        </style>
     </head>
     <body>
         <div class="reg1">
@@ -11,7 +18,7 @@
                 include('connect-mysql.php');
                 $name = $_SESSION['username'];
                 if (!$_SESSION['username']) {
-                    header('location:index.php');
+                    header('location:login.php');
                 } else {
                     echo "<h1>Welcome," . $_SESSION['username'] . "!</h1>";
                 }
@@ -32,6 +39,7 @@
                     $mobile = $rows['user_mobile'];
                     $gender = $rows['user_gender'];
                     $address = $rows['user_address'];
+                    $pic=$rows['user_pic'];
                     $date = $rows['user_date'];
 
 
@@ -41,11 +49,13 @@
                     echo "<div class='namefield'><label>Mobile:</label>  $mobile</div>";
                     echo "<div class='namefield'><label>Gender:</label>  $gender</div>";
                     echo "<div class='namefield'><label>Address:</label>  $address</div>";
+                    echo "<div clas='namefield'><label>Picture:</label><img src='$pic'></div>";
                     echo "<div class='namefield'><label>Date of join:</label>  $date</div>";
 
                      }
                 ?>
             </div>
+            
             <a href='logout.php'><button>Logout</button></a>
         </div>
 
